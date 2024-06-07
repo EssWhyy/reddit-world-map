3be02d45c0a493500e5474c6b9f1767385e77949
EssWhyy <sventang1@gmail.com>
2024-06-07 12:32:13 +0800

    Add base color coding

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const projection = d3.geoNaturalEarth1()
     .scale(width / 5)
     .translate([width / 2, height / 2])
 
+// Opacity tooltip
+
+const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
 // Load external data and boot
 d3.json("world-map.geojson").then( function(data) {
 
@@ -16,10 +22,21 @@ d3.json("world-map.geojson").then( function(data) {
         .selectAll("path")
         .data(data.features)
         .join("path")
-            .attr("fill", "#69b3a2")
+            .attr("fill", "orange")
             .attr("d", d3.geoPath()
             .projection(projection)
             )
             .style("stroke", "#fff")
+            .on("mouseover", (event, d) => {
+                d3.select(event.currentTarget).style("fill", "lightsalmon");
+                tooltip.transition().duration(200).style("opacity", .5);
+                tooltip.html(d.properties.name)
+                    .style("left", (event.pageX) + "px")
+                    .style("top", (event.pageY - 28) + "px");
+            })
+            .on("mouseout", (event, d) => {
+                d3.select(event.currentTarget).style("fill", "");
+                tooltip.transition().duration(200).style("opacity", 0);
+            });
 
 })
